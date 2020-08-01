160632361401046102872b7e71355c109b7607b6
wangweiqi <799819401@qq.com>
2020-08-01 23:51:17 +0800

    update

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -7,12 +7,12 @@ export jmx_template_filename="${jmx_template}${suffix}"
 export os_type=`uname`
 
 # 需要在系统变量中定义jmeter根目录的位置，如下
-# export jmeter_path="/your jmeter path/"
+export jmeter_path="/Users/wangweiqi/Downloads/apache-jmeter-5.3/"
 
 echo "自动化压测开始"
 
 # 压测并发数列表
-thread_number_array=(10 20 30 40 50)
+thread_number_array=(10 20 30)
 for num in "${thread_number_array[@]}"
 do
     # 生成对应压测线程的jmx文件
